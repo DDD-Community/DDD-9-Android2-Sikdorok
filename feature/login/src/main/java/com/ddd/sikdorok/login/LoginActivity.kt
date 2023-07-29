ef5a219c5d2347b0aa0b36768de2dd3e0fad9110
taeseongyun <yts0646@gmail.com>
2023-07-29 17:36:25 +0900

    [ Feature ] : 로그인 로직 기능 구현 완료 ( 카카오 )

--- feature/login/src/main/java/com/ddd/sikdorok/login/LoginActivity.kt
+++ feature/login/src/main/java/com/ddd/sikdorok/login/LoginActivity.kt
@@ -1,22 +1,60 @@
 package com.ddd.sikdorok.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.ddd.sikdorok.login.databinding.ActivityLoginBinding
 import com.ddd.sikdorok.navigator.signin.SignInNavigator
+import com.example.core_ui.base.BaseActivity
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
     @Inject
     lateinit var signInNavigator: SignInNavigator
+
+    override val viewModel by viewModels<LoginViewModel>()
+    override fun initLayout() {}
+
+    private val callback: (token: OAuthToken?, error: Throwable?) -> Unit = { token, error ->
+        if(error != null) {
+            Log.e(TAG, "카카오 로그인 실패")
+        } else if(token != null) {
+            viewModel.event(LoginContract.Event.CheckKakaoUser(token.accessToken))
+        }
+    }
+
+    override fun setupCollect() {
+        viewModel.effect
+            .flowWithLifecycle(lifecycle, Lifecycle.State.CREATED)
+            .onEach { effect ->
+                when(effect) {
+                    is LoginContract.SideEffect.NaviToSikdorokLogin -> {
+                        startActivity(signInNavigator.start(this))
+                    }
+                    is LoginContract.SideEffect.NaviToSignUp -> {
+                        Log.e("result", effect.email.orEmpty())
+                    }
+                    is LoginContract.SideEffect.NaviToKakaoLogin -> {
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    }
+                    is LoginContract.SideEffect.NaviToHome -> {
+                        finish()
+                    }
+                }
+            }
+            .launchIn(lifecycleScope)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +64,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.viewEmail.setOnClickListener {
-            startActivity(signInNavigator.start(this))
-        }
-
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Log.e(TAG, "카카오계정으로 로그인 실패", error)
-            } else if (token != null) {
-                Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
-            }
+            viewModel.event(LoginContract.Event.RequestSikdorokLogin)
         }
 
         binding.viewKakao.setOnClickListener {
-            UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            viewModel.event(LoginContract.Event.RequestKakaoLogin)
         }
-
-        val hashkey = Utility.getKeyHash(this)
-        Log.e("hash", hashkey)
     }
 
     override fun onDestroy() {
