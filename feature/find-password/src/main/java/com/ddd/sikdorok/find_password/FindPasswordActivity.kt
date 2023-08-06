1ee2fc20ff04e573a8bc62fa3672735359ca2162
taeseongyun <yts0646@gmail.com>
2023-08-06 23:43:23 +0900

    [ Feature ] : 식도록 비밀번호 찾기 디자인 구현 완료

--- feature/find-password/src/main/java/com/ddd/sikdorok/find_password/FindPasswordActivity.kt
+++ feature/find-password/src/main/java/com/ddd/sikdorok/find_password/FindPasswordActivity.kt
@@ -1,11 +1,15 @@
 package com.ddd.sikdorok.find_password
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
+import com.ddd.sikdorok.find_password.databinding.ActivityFindPasswordBinding
+import com.example.core_ui.base.BaseActivity
+import com.example.core_ui.base.BaseViewModel
 
-class FindPasswordActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_password)
-    }
+class FindPasswordActivity : BaseActivity<ActivityFindPasswordBinding>(ActivityFindPasswordBinding::inflate) {
+
+    override val viewModel: BaseViewModel by viewModels()
+
+    override fun initLayout() {}
+
+    override fun setupCollect() {}
 }
