2ffcedcf192462e6ba2838103ac1ff59bb23b9df
taeseongyun <yts0646@gmail.com>
2023-08-13 10:38:30 +0900

    [ Feature ] : 생성 및 수정 피쳐 모듈 베이스화

--- feature/modify/src/main/java/com/ddd/sikdorok/modify/ModifyActivity.kt
+++ feature/modify/src/main/java/com/ddd/sikdorok/modify/ModifyActivity.kt
@@ -1,11 +1,25 @@
 package com.ddd.sikdorok.modify
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.viewModels
+import com.ddd.sikdorok.core_ui.base.BackFrameActivity
+import com.ddd.sikdorok.core_ui.base.BaseViewModel
+import com.ddd.sikdorok.modify.databinding.ActivityModifyBinding
 
-class ModifyActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify)
+class ModifyActivity : BackFrameActivity<ActivityModifyBinding>(ActivityModifyBinding::inflate) {
+
+    override val backFrame: FrameLayout by lazy { binding.frameBack }
+
+    override val viewModel: BaseViewModel by viewModels()
+
+    override fun initLayout() {
+
+    }
+
+    override fun onClickBackFrameIcon() {
+
+    }
+
+    override fun setupCollect() {
     }
 }
