a22f854a3dd4a8d7c30b521901cf25390e425ab1
TaeseongYun <yts0646@gmail.com>
2023-08-20 18:37:16 +0900

    [ Feature ] : 계정삭제 피쳐 모듈 연결 (#32)
    * [ Feature ] : 계정삭제 피쳐 모듈 생성완료
    
    * [ Feature ] : 계정삭제 네비게이터 연결 완료

--- app/src/main/java/com/ddd/sikdorok/navigator/DeleteAccountNavigatorImpl.kt
+++ app/src/main/java/com/ddd/sikdorok/navigator/DeleteAccountNavigatorImpl.kt
@@ -2,11 +2,12 @@ package com.ddd.sikdorok.navigator
 
 import android.content.Context
 import android.content.Intent
+import com.ddd.sikdorok.delete_account.DeleteAccountActivity
 import com.ddd.sikdorok.navigator.delete_account.DeleteAccountNavigator
 import javax.inject.Inject
 
 internal class DeleteAccountNavigatorImpl @Inject constructor(): DeleteAccountNavigator {
     override fun start(context: Context): Intent {
-        return Intent()
+        return Intent(context, DeleteAccountActivity::class.java)
     }
 }
